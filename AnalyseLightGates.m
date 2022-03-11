5567d96aa0c435b005ac73609b5c63275b4fb5c9
Liam Smith <liam.smith@eng.ox.ac.uk>
2022-03-11 16:37:34 +0000

    Update for release

--- AnalyseLightGates.m
+++ AnalyseLightGates.m
@@ -1,16 +1,29 @@
-MyPath1 = 'Path to lg1 .trc file';
-MyPath2 = 'Path to lg1 .trc file';
 
+MyPath1 = 'Path to lg1 scope file';
+MyPath2 = 'Path to lg2 scope file';
 
-LightGate1 = LightGate('FilePath',MyPath1).FitLightGate;
-LightGate2 = LightGate('FilePath',MyPath2).FitLightGate;
+% Seed LightGate objects using above file paths
+LightGate1 = LightGate('FilePath',MyPath1);
+LightGate2 = LightGate('FilePath',MyPath2);
 
+% Fit LightGate signals
+LightGate1 = LightGate1.FitLightGate;
+LightGate2 = LightGate2.FitLightGate;
+
+% Pull out the results of the FitLightGate method.
 LG1Time = LightGate1.TrigTime;
 LG2Time = LightGate2.TrigTime;
-% 2 Element Array with time and error (in seconds) for each light gate [Trig Time, 95% Error].
+% LG1Time/LG2Time are a 2-Element arrays with trigger time and the 
+% asscoiated error (both in seconds). The error is the 95% Confidence
+% interval width (2 Standard Deviations) of the predicted trigger time.
 
-Distance = [12e-3,100e-6];
+Distance    = nan(2,1);
+Distance(1) = 12e-3;  % Light Gate Separation Value [m]
+Distance(2) = 100e-6; % Light Gate Separation Error [m]
+
+% Transit Time
 Time     = [abs(LG1Time(1)-LG1Time(2)) sqrt(LG1Time(2)^2 + LG2Time(2)^2)];
 
-Velocity = Distance / Time;
+% Velocity and Velocity Error
+Velocity = Distance(1) / Time(1);
 VelocityError = Velocity * sqrt((Distance(2)/Distance(1))^2+(Time(2)/Time(1))^2);
--- LightGate.m
+++ LightGate.m
@@ -1,12 +1,38 @@
 classdef LightGate
-    %UNTITLED5 Summary of this class goes here
-    %   Detailed explanation goes here
+    % An object to store and analyse light gate data
+    %   Input Args (All given as name-value):
+    %       'FilePath' - String  - The path to an oscilloscope file that
+    %                              corresponds to a light gate.
+    %       'Delay'    - Numeric - The amount of cable delay this trace was
+    %                              subjected to, this is to be given in the
+    %                              same units as the trace itself (It will
+    %                              delay the time axis by that value).
+    %   Methods:
+    %       FitLightGate: This method actually fits the light gate trace,
+    %                     it will ask you to draw a rectangle around the
+    %                     region where the light gate falls. It will return
+    %                     FittedCurve, TrigTime, & Box properties. You can
+    %                     specify Box in future analyses to use reuse the 
+    %                     previously made box.
+    %
+    %
+    %   Dependencies:
+    %       ScopeTrace: This is contained within the ImportScope repo, if
+    %                   you have installed both LightGateToolbox & 
+    %                   ImportScope repos in the same folder then the 
+    %                   default value property ScopeTracePath
+    %                   ('../ImportScope') should satisfy this dependency,
+    %                   if you have not you should edit this to provide a
+    %                   correct path to the ImportScope function.
+
     
     properties
         RawData
+        ScopeTracePath = '../ImportScope'
         Delay
         FittedCurve
         TrigTime
+        Box
     end
     properties (Dependent)
         Voltage
@@ -19,13 +45,13 @@ classdef LightGate
                 inputargs.FilePath  = 'Undefined'
                 inputargs.Delay     = 0
             end
-            
+            addpath(obj.ScopeTracePath)
             if isfile(inputargs.FilePath)
                 obj.RawData = ScopeTrace('FilePath',inputargs.FilePath);
             end
             obj.Delay = inputargs.Delay;
+            path(obj.ScopeTracePath)
         end
-        
         function obj = FitLightGate(obj,Box,plotflag)
             arguments
                 obj
@@ -34,27 +60,24 @@ classdef LightGate
             end
             
             Signal = rescale(obj.Voltage);
-            tmp = rescale(movmean(Signal,20));
-            Crossover = median(find(tmp>0.475 & tmp<0.525));
             
+            tmp         = rescale(movmean(Signal,20));
+            Crossover   = median(find(tmp>0.475 & tmp<0.525));
             
             if ischar(Box)
                 plot(tmp);
                 title('Drag rectangle over transition region')
                 Box = getrect();
                 close all
-%                 disp('Box:')
-%                 fprintf('%.0f\n',round(Box(1)))
-%                 fprintf('%.f\n',Box(2))
-%                 fprintf('%.0f\n',round(Box(3)))
-%                 fprintf('%.f\n',Box(4))
                 plotflag = true;
             end
-            
+
+            obj.Box = Box;
+
             PreSignalLevel  = median(Signal(1:round(Box(1))));
             PostSignalLevel = median(Signal(round(Box(1)+Box(3)):end));
-            Amplitude = range([PreSignalLevel,PostSignalLevel])/2;
-            %fitting erfc function to reduced space
+            Amplitude       = range([PreSignalLevel,PostSignalLevel])/2;
+            
             [xData, yData] = prepareCurveData([],Signal);
             
             opts             = fitoptions('Method','NonlinearLeastSquares');
@@ -75,9 +98,11 @@ classdef LightGate
                                                'dependent',...
                                                'y'),...
                                        opts );
+
             TriggerIdx = coeffvalues(obj.FittedCurve);
             TriggerIdx = TriggerIdx(3);
             obj.TrigTime = interp1(1:numel(obj.Time),obj.Time,TriggerIdx);
+
             ErrorVal = confint(obj.FittedCurve);
             ErrorVal = range(ErrorVal(:,3))*(obj.RawData.Time(2)-obj.RawData.Time(1));
             obj.TrigTime(2) = ErrorVal/2;
@@ -93,10 +118,14 @@ classdef LightGate
             end
         end
         function Time = get.Time(obj)
+            addpath(obj.ScopeTracePath)
             Time = obj.RawData.Time - obj.Delay;
+            rmpath(obj.ScopeTracePath)
         end
         function Voltage = get.Voltage(obj)
+            addpath(obj.ScopeTracePath)
             Voltage = obj.RawData.Voltage;
+            rmpath(obj.ScopeTracePath)
         end
     end
 end
